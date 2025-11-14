9f83b210234af05cfce1e778a99102dc526422db
sulakshana kalugamage <sulakshana.sus@gmail.com>
2025-11-14 23:20:41 +0530

    commments

--- src/App.jsx
+++ src/App.jsx
@@ -71,6 +71,7 @@ export default function AssessmentPortfolio() {
       </div>
 
       {/* CONTENT AREA WITH MODERN CARDS */}
+      {/*mewa wens nokranuuuu  */}
       <main className="max-w-5xl mx-auto px-4 md:px-6 py-12 space-y-12">
         {chapters.map((c) => {
           const setRef = (el) => (sectionRefs.current[c.id] = el);
@@ -106,7 +107,7 @@ export default function AssessmentPortfolio() {
                 );
             }
           };
-
+          {/*mewa wens nokranuuuu  */}
           return (
             <section
               key={c.id}
